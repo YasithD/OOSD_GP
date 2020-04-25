f55bc51fc6f192441f0af82cbda5958a31e55cef
D-sandaruwan <dsandaruwan7@gmail.com>
2020-04-25 19:29:07 +0530

    completed the main-nav

--- codeblocks/main-nav.php
+++ codeblocks/main-nav.php
@@ -1,8 +1,12 @@
-<nav class="main-nav">
+<nav class="main-nav nav">
     <div class="square header-logo_container">
         <img src="images/police-logo.png" alt="nav bar logo">
     </div>
     <div class="square square-2"></div>
     <div class="square square-3"></div>
     <div class="square square-4"></div>
+    <ul class="navbar-nav ml-auto log-sec">
+        <li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-fw fa-user"></i>My Profile</a></li>
+        <li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-fw fa-sign-out-alt"></i>Logout</a></li>
+    </ul> 
 </nav>
--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require_once("codeblocks/meta.php")?>
+    <script src="js/jquery-3.5.0.js"></script>    
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <?php require_once("codeblocks/css.php")?>
     <link href="css/style.css" rel="stylesheet">
-    <link href="css/navigation.css" rel="stylesheet">
     <title>MOR</title>
-</head>
+</script>
 <body>
     <header class="header">
         <?php require_once("codeblocks/main-nav.php")?>
